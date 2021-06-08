b9a5f28e3c8dfaa52ed6ee66b87e4e9fe485c407
alinidhal <ali.nidhal@hotmail.com>
2021-06-08 11:26:58 +0200

    closes #7; HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $postRepo = $this->getDoctrine()->getRepository(Post::class);
+
+        $items = $postRepo->findAll();
+        return $this->render('home/index.html.twig', [
+            'items' => $items,
+        ]);
+    }
+}
